cd5ebd273234e4ff95da6f5f8520a718d103205e
aintnofastlife <yusukemiyamura2025@gmail.com>
2023-11-19 21:37:03 +0900

    Homeにそれぞれのアプリに飛ぶリンクを設置しました

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,9 @@
 <template>
   <h1>Home</h1>
   <div>ここにそれぞれのアプリに行けるリンクを設置する</div>
+  <a href="/quiz">→クイズアプリ</a>
+  <br />
+  <a href="/pallete">→パレットアプリ</a>
+  <br />
+  <a href="/memo">→メモアプリ</a>
 </template>
