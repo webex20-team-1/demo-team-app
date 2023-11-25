f5076e72f1bbc6008de2921f143f51b7de8e1bcc
kanamitakashima <tk4mk7367@outlook.jp>
2023-11-25 22:25:20 +0900

    クイズアプリの編集中です

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,26 +1,63 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ "クイズタイトル" }}</h2>
+    <h2>Q. {{ quiz.text }}</h2>
     <img
       class="quiz-image"
       src="https://via.placeholder.com/300x300"
-      alt="クイズタイトル"
+      v-bind:alt="quiz.text"
     />
     <div class="container">
       <button>
-        {{ "選択肢1" }}
+        {{ quiz.choices[0].text }}
       </button>
       <button>
-        {{ "選択肢2" }}
+        {{ quiz.choices[1].text }}
       </button>
       <button>
-        {{ "選択肢3" }}
+        {{ quiz.choices[2].text }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div>{{ quiz }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      quiz: {
+        text: "チーム名に含まれる「dlbia」はイギリスのバンドOasisによる「Don't Look Back In Anger」の略称です。この英文の意味は？",
+        image: "",
+      },
+    }
+  },
+}
+</script>
+
+<style>
+.app {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.quiz-image {
+  height: 300px;
+  width: 300px;
+  object-fit: contain;
+}
+
+.container {
+  display: flex;
+  height: 2em;
+  width: 300px;
+  padding: 1em;
+  justify-content: space-around;
+}
+</style>
 
 <script>
 export default {}
