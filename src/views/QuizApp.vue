ddecb0eec153f4091b643214ecffcc69216e665a
kanamitakashima <tk4mk7367@outlook.jp>
2023-11-26 15:29:47 +0900

    クイズアプリの編集中です

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -4,21 +4,19 @@
     <h2>Q. {{ quiz.text }}</h2>
     <img
       class="quiz-image"
-      src="https://via.placeholder.com/300x300"
+      v-bind:src="oasis_morningglory.jpg"
       v-bind:alt="quiz.text"
     />
     <div class="container">
-      <button>
-        {{ quiz.choices[0].text }}
-      </button>
-      <button>
-        {{ quiz.choices[1].text }}
-      </button>
-      <button>
-        {{ quiz.choices[2].text }}
+      <button
+        v-for="(choice, i) in quiz.choices"
+        v-bind:key="i"
+        v-on:click="choiced(choice)"
+      >
+        {{ choice.text }}
       </button>
     </div>
-    <div>{{ quiz }}</div>
+    <div>{{ feedback }}</div>
   </div>
 </template>
 
@@ -26,11 +24,36 @@
 export default {
   data: function () {
     return {
+      feedback: "",
       quiz: {
         text: "チーム名に含まれる「dlbia」はイギリスのバンドOasisによる「Don't Look Back In Anger」の略称です。この英文の意味は？",
         image: "",
+        choices: [
+          {
+            text: "ゴリアテ",
+            isCorrect: false,
+            feedback:
+              "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+          },
+          {
+            text: "ゼニガメ",
+            isCorrect: false,
+            feedback:
+              "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+          },
+          {
+            text: "ガニメデ",
+            isCorrect: true,
+            feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+          },
+        ],
       },
     }
+  },
+  methods: {
+    choiced(choice) {
+      this.feedback = choice.feedback
+    },
   },
 }
 </script>
@@ -58,10 +81,6 @@ export default {
   justify-content: space-around;
 }
 </style>
-
-<script>
-export default {}
-</script>
 
 <style>
 .app {
